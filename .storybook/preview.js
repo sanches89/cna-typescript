7dbf9a316462e5aa9439d6c86f06618c9425db65
Ivan Sanches <sanches.ic.89@gmail.com>
2020-10-28 00:26:40 -0400

    fix: not use default exports

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,6 +1,6 @@
-import { ThemeProvider } from 'styled-components';
+import {ThemeProvider} from 'styled-components';
 
-import theme from '../src/styles/theme'
+import {theme} from '../src/styles/theme'
 
 export const decorators = [
   (Story) => (
