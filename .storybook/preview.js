90f5ce088962e84a82cd0655067db456c16cc92c
Ivan Sanches <sanches.ic.89@gmail.com>
2020-12-18 08:53:27 -0400

    fix: add css reset at storebook

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -2,6 +2,8 @@ import {ThemeProvider} from 'styled-components'
 
 import {theme} from '../src/styles/theme'
 
+import 'modern-css-reset/dist/reset.css'
+
 export const decorators = [
   (Story) => (
     <ThemeProvider theme={theme}>
