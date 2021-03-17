1d93980738a5b044d496c44afbbac9d4d7ed34b9
--global <sanches.ic.89@gmail.com>
2021-03-16 22:08:51 -0300

    📦 image config

--- next.config.js
+++ next.config.js
@@ -5,19 +5,15 @@ module.exports = withPWA({
   // https://nextjs.org/docs/api-reference/next.config.js/react-strict-mode
   reactStrictMode: true,
 
+  // next/image
+  images: {
+    deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
+  },
+
   // next/pwa default config
   pwa: {
     disable: process.env.NODE_ENV === 'development',
     dest: 'public',
     runtimeCaching,
-  },
-
-  // default config of next/image
-  images: {
-    deviceSizes: [320, 420, 768, 1024, 1200],
-    iconSizes: [],
-    domains: [],
-    path: '/_next/image',
-    loader: 'default',
   },
 })
