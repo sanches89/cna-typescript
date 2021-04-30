534272afc62e156251e99320b9b5ed1153294699
--global <sanches.ic.89@gmail.com>
2021-04-30 01:28:26 -0300

    🧹 change e2e to another folder

--- jest.e2e.config.js
+++ jest.e2e.config.js
@@ -1,5 +1,5 @@
 module.exports = {
   preset: "jest-playwright-preset",
   setupFilesAfterEnv: ["expect-playwright"],
-  testMatch: ["**/?(*.)+(e2e).ts?(x)"],
+  testMatch: ["**/__e2e__/*.ts?(x)"],
 }
